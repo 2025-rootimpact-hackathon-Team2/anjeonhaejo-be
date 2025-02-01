82e5d7f1360af1706d5ea67477038c3f8a6d547e
jungsehui <jungsehui@naver.com>
2025-02-01 19:22:50 +0900

    initial project

---
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/AnjeonhaejoApplication.java
@@ -0,0 +1,13 @@
+package com.rootimpact.anjeonhaejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnjeonhaejoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnjeonhaejoApplication.class, args);
+    }
+
+}
---
+++ anjeonhaejo/src/test/java/com/rootimpact/anjeonhaejo/AnjeonhaejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootimpact.anjeonhaejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnjeonhaejoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
