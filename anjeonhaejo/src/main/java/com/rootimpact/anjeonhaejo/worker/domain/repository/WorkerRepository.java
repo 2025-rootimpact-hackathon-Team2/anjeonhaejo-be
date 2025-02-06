0a988376527213bbb52c91db5fdd47437f32f169
jungsehui <jungsehui@naver.com>
2025-02-06 22:37:06 +0900

    feat: WorkerRepository(작업자 레포지토리) 전체 반환 구현

--- anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/worker/domain/repository/WorkerRepository.java
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/worker/domain/repository/WorkerRepository.java
@@ -3,7 +3,9 @@ package com.rootimpact.anjeonhaejo.worker.domain.repository;
 import com.rootimpact.anjeonhaejo.worker.domain.entity.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
 
-
+    List<Worker> findByWorkerId();
 }
