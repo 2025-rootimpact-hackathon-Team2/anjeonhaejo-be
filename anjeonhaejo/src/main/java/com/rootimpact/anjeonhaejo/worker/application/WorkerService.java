53c95aa9b674b1731e1984c8e4a87b416151d9fc
jungsehui <jungsehui@naver.com>
2025-02-06 22:56:04 +0900

    feat: 전체 작업자 반환 메서드 구현

--- anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/worker/application/WorkerService.java
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/worker/application/WorkerService.java
@@ -1,8 +1,13 @@
 package com.rootimpact.anjeonhaejo.worker.application;
 
+import com.rootimpact.anjeonhaejo.worker.domain.entity.Worker;
 import com.rootimpact.anjeonhaejo.worker.domain.repository.WorkerRepository;
+import com.rootimpact.anjeonhaejo.worker.presentation.dto.response.ReadAllWorkersResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +15,11 @@ public class WorkerService {
 
     private final WorkerRepository workerRepository;
 
-
+    @Transactional
+    public List<ReadAllWorkersResponse> showAllWorkers() {
+        return workerRepository.findAll()
+                .stream()
+                .map(ReadAllWorkersResponse::from)
+                .toList();
+    }
 }
