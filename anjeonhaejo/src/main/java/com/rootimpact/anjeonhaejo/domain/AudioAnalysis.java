1d0f8d14e0e16cff78520526fb3141d7004f767a
임태연 <limtaeyeon@imtaeyeon-ui-MacBookAir.local>
2025-02-09 21:33:35 +0900

    음성인식 ai 형식 변경

--- anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/domain/AudioAnalysis.java
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/domain/AudioAnalysis.java
@@ -9,20 +9,25 @@ import java.util.List;
 @Entity
 @Table(name = "audio_analysis")
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class AudioAnalysis {
+public class AudioAnalysis extends BaseTimeEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String filename;
 
+    private String workerZone;
+
+    private double decibel;
+
     private String soundClass;
 
     private String transcription;
+
+
 
     @ElementCollection
     private List<String> detectedKeywords; // List로 변환된 값을 저장
---
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/responseDTO/EmergencyDecibelResponseDTO.java
@@ -0,0 +1,29 @@
+package com.rootimpact.anjeonhaejo.responseDTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class EmergencyDecibelResponseDTO {
+
+    private LocalDateTime timestamp;
+
+    private String workerZone;
+
+    private double decibel;
+
+    private String soundClass;
+
+    private String transcription;
+
+    public EmergencyDecibelResponseDTO(LocalDateTime timestamp, String workerZone, double decibel, String soundClass, String transcription) {
+        this.timestamp = timestamp;
+        this.workerZone = workerZone;
+        this.decibel = decibel;
+        this.soundClass = soundClass;
+        this.transcription = transcription;
+    }
+}
