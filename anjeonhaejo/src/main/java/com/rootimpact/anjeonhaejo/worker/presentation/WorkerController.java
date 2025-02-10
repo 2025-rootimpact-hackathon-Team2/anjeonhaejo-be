fd47475167af53066d474dece76d8964eb45c10b
jungsehui <jungsehui@naver.com>
2025-02-10 18:42:09 +0900

    feat: 작업자 생성 API 구현

--- anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/worker/presentation/WorkerController.java
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/worker/presentation/WorkerController.java
@@ -2,14 +2,15 @@ package com.rootimpact.anjeonhaejo.worker.presentation;
 
 import com.rootimpact.anjeonhaejo.worker.application.WorkerService;
 import com.rootimpact.anjeonhaejo.worker.domain.entity.Worker;
+import com.rootimpact.anjeonhaejo.worker.presentation.dto.request.CreateWorkerRequest;
 import com.rootimpact.anjeonhaejo.worker.presentation.dto.response.ReadAllWorkersResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -21,7 +22,12 @@ public class WorkerController {
 
     private final WorkerService workerService;
 
-
+    @PostMapping("/workers")
+    public ResponseEntity<Void> createWorker(@RequestBody CreateWorkerRequest createWorkerRequest) {
+        log.info("method: createWorker, requested time: {}", LocalDateTime.now());
+        Long workerId = workerService.registerWorker(createWorkerRequest);
+        return ResponseEntity.created(URI.create("/api/v1/workers" + workerId)).build();
+    }
 
     @GetMapping("/workers")
     public ResponseEntity<List<ReadAllWorkersResponse>> readAllWorkers() {
