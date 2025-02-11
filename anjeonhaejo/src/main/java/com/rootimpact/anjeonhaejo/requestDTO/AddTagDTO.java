15fff763f80b214ebaddb9a957c9722ab9cde1bc
jungsehui <jungsehui@naver.com>
2025-02-11 17:07:10 +0900

    feat: request dto 부분 스키마 설정

--- anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/requestDTO/AddTagDTO.java
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/requestDTO/AddTagDTO.java
@@ -1,12 +1,15 @@
 package com.rootimpact.anjeonhaejo.requestDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class AddTagDTO {
 
+    @Schema(description = "태그 이름", example = "소음 이상")
     private String tagName;
 
+    @Schema(description = "태그 카테고리", example = "기계")
     private String category;
 
     public AddTagDTO(String tagName, String category) {
--- anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/requestDTO/LoginDTO.java
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/requestDTO/LoginDTO.java
@@ -1,12 +1,15 @@
 package com.rootimpact.anjeonhaejo.requestDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class LoginDTO {
 
+    @Schema(description = "로그인 아이디(이메일)", example = "user2@example.com")
     private String email;
 
+    @Schema(description = "로그인 비밀번호", example = "1234")
     private String password;
 
     public LoginDTO(String email, String password) {
