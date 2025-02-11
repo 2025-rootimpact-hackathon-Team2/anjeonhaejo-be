72f551392edcadc542a6dd71013107f7c3c4b588
jungsehui <jungsehui@naver.com>
2025-02-11 16:58:52 +0900

    feat: 엔드포인트 swagger 적용

--- anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/controller/AudioController.java
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/controller/AudioController.java
@@ -1,8 +1,10 @@
 package com.rootimpact.anjeonhaejo.controller;
 
-import com.rootimpact.anjeonhaejo.domain.AudioAnalysis;
 import com.rootimpact.anjeonhaejo.responseDTO.EmergencyDecibelResponseDTO;
 import com.rootimpact.anjeonhaejo.service.AudioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,11 @@ public class AudioController {
 
     private final AudioService audioService;
 
+    @Operation(summary = "오디오 생성", description = "오디오를 업로드합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "오디오 업로드 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터")
+    })
     @PostMapping("/upload")
     public ResponseEntity<EmergencyDecibelResponseDTO> uploadAudio(@RequestParam("file") MultipartFile file,
                                                      @RequestParam("decibel") double decibel,
@@ -31,6 +38,11 @@ public class AudioController {
         }
     }
 
+    @Operation(summary = "전체 오디오 조회", description = "전체 오디오를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "오디오 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터")
+    })
     @GetMapping("/all")
     public ResponseEntity<List<EmergencyDecibelResponseDTO>> getAllAudioFiles() {
         return ResponseEntity.ok(audioService.getAllAudioFiles());
--- anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/controller/ReportController.java
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/controller/ReportController.java
@@ -4,6 +4,9 @@ import com.rootimpact.anjeonhaejo.requestDTO.CreateReportRequestDTO;
 import com.rootimpact.anjeonhaejo.responseDTO.CreateReportResponseDTO;
 import com.rootimpact.anjeonhaejo.security.custom.CustomUserDetails;
 import com.rootimpact.anjeonhaejo.service.ReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +20,21 @@ public class ReportController {
 
     private final ReportService reportService;
 
+    @Operation(summary = "레코드 생성", description = "레코드를 추가합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "레코드 추가 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터")
+    })
     @PostMapping("/create")
     public String addRecord(@RequestBody CreateReportRequestDTO dto, @AuthenticationPrincipal CustomUserDetails user) throws ChangeSetPersister.NotFoundException {
         return reportService.createReport(user.getUserId(), dto);
     }
 
+    @Operation(summary = "리포트 조회", description = "단일 상세 리포트를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "리포트 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터")
+    })
     @GetMapping("/show/detail/{reportId}")
     public ResponseEntity<CreateReportResponseDTO> showDetailReport(@PathVariable("reportId") Long reportId, @AuthenticationPrincipal CustomUserDetails user) {
         return reportService.showDetailReport(reportId, user.getUserId());
