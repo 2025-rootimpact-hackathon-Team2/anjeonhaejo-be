ab2271a8b849b7851851c765fbe19bbcb3fa7aee
jungsehui <jungsehui@naver.com>
2025-02-11 17:07:10 +0900

    feat: request dto 부분 스키마 설정

--- anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/requestDTO/CreateReportRequestDTO.java
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/requestDTO/CreateReportRequestDTO.java
@@ -1,5 +1,6 @@
 package com.rootimpact.anjeonhaejo.requestDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.util.List;
@@ -7,12 +8,16 @@ import java.util.List;
 @Getter
 public class CreateReportRequestDTO {
 
+    @Schema(description = "리포트 제목", example = "222")
     private String title;
 
+    @Schema(description = "컨텐츠", example = "22")
     private String content;
 
+    @Schema(description = "작업 라인 아이디", example = "3")
     private Long workerLineId;
 
+    @Schema(description = "태그 아이디 목록", example = "[2]")
     private List<Long> tagIds;  // 태그 ID 목록
 
 }
