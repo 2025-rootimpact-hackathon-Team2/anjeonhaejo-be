c5e4637a86460ae6a9010a68452c6a0d3315c814
jungsehui <jungsehui@naver.com>
2025-02-11 17:36:02 +0900

    feat: response dto 부분 스키마 설정

--- anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/responseDTO/ShowStateResponseDTO.java
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/responseDTO/ShowStateResponseDTO.java
@@ -1,6 +1,7 @@
 package com.rootimpact.anjeonhaejo.responseDTO;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,13 +13,17 @@ import java.util.List;
 @Builder
 public class ShowStateResponseDTO {
 
+    @Schema(description = "구역 이름", example = "소음 이상")
     private String zoneName;
 
+    @Schema(description = "구역 이름", example = "냉각기")
     private List<String> machineName;
 
+    @Schema(description = "임계값", example = "85db")
     private int threshold;
 
-    private String state; // 0-2회 정상, 3-5회 주의, 6회 이상 비정상
+    @Schema(description = "상태", example = "0-2회 정상, 3-5회 주의, 6회 이상 비정상")
+    private String state; //
 
     public ShowStateResponseDTO(String zoneName, List<String> machineName, int threshold, String state) {
         this.zoneName = zoneName;
