27e33354cf6996f6dbf7887d4d18faae6940e859
jungsehui <jungsehui@naver.com>
2025-02-12 20:18:59 +0900

    feat: CORS 오픈 설정

--- anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/global/WebMvcConfig.java
+++ anjeonhaejo/src/main/java/com/rootimpact/anjeonhaejo/global/WebMvcConfig.java
@@ -1,4 +1,20 @@
 package com.rootimpact.anjeonhaejo.global;
 
-public class WebMvcConfig {
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
